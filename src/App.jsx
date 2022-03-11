1830f49f53f2edde61cf189c1a05965231627ae7
Karl Yang <ccok12345678@gmail.com>
2022-03-11 15:23:56 +0800

    added TodoTable

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import ContentBoard from "./components/ContentBoard"
 import Logo from "./components/Logo"
+import TodoTable from "./components/TodoTable"
 
 function App() {
   return (
     <div className="container">
       <ContentBoard
-        logo={ <Logo /> } />
+        logo={<Logo />}
+        todoTable={<TodoTable />}
+      />
     </div>
   )
 }
---
+++ src/components/TodoTable.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class TodoTable extends Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default TodoTable
