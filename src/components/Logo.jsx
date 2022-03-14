9b19011a12c86913685ee4c0e1ff881a623e6998
Karl Yang <ccok12345678@gmail.com>
2022-03-14 14:11:36 +0800

    fixed logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,11 @@
+import logo from '../images/logo.svg'
+
 function Logo() {
   return (
     <a href="#" title="農場備忘" className="logo">
       <h1 className="logo-title">農場備忘</h1>
       <img
-        src="src/images/logo.svg"
+        src={logo}
         alt="logo"
         className="logo-img"/>
     </a>
